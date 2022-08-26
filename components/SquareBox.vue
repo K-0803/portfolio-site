5b974bc2efe6367eb8c6dea7b76eb619bb33bde8
K-0803 <108750099+K-0803@users.noreply.github.com>
2022-08-26 13:51:05 +0900

    Initial commit

---
+++ components/SquareBox.vue
@@ -0,0 +1,40 @@
+<template>
+<div 
+        class = "square"
+        :style = "{background: activeColor, content:anyText}"
+        @click = "changeColor">
+      </div>
+
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial',
+  props: ["color1", "text"],
+  data(){
+    return{
+      activeColor: this.color1,
+      anyText: this.text,
+    }
+  },
+  methods:{
+    changeColor(){
+      /* if(this.activeColor === this.color1){
+        this.activeColor = this.color2
+      }else if(this.activeColor === this.color2){
+        this.activeColor = this.color3
+      }else{
+        this.activeColor = this.color1
+      } */
+    }
+  },
+}
+</script>
+
+<style>
+.square{
+  width: 75px;
+  height: 50px;
+}
+
+</style>
