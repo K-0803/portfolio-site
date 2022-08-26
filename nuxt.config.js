6f68dd573bd53dea5b6f14901087a8457cc59f91
K-0803 <108750099+K-0803@users.noreply.github.com>
2022-08-26 13:51:05 +0900

    Initial commit

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,20 +10,21 @@ export default {
     title: 'portfolio_site',
     htmlAttrs: {
       lang: 'en',
+      prefix: "og: http://ogp.me/ns#"
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
-      { hid: 'og:type', property: 'og:type', content: 'article' },
+      { hid: 'og:type', property: 'og:type', content: 'website' },
       { hid: 'og:locale', property: 'og:locale', content: 'ja_JP' },
       { hid: 'og:site_name', property: 'og:site_name', content: 'portfolio_site' },
       { hid: 'twitter:card', name: 'twitter:card', content: 'summary_large_image' },
-      /* { hid: 'og:title', property: 'og:title', content: ('portfolio_site') },
-      { hid: 'og:image', property: 'og:image', content: ('static/20220826_screenshot.png') },
-      { hid: 'og:description', property: 'og:description', content: (サイトの説明) }, */
-      { hid: 'og:url', property: 'og:url', content: ('https://portfolio-site-4kg.pages.dev/')},
+      { hid: 'og:title', property: 'og:title', content: 'portfolio_site' },
+      { hid: 'og:image', property: 'og:image', content: '' },
+      { hid: 'og:description', property: 'og:description', content: ('このサイトはSugimoto Kazumasaのポートフォリオサイトです。') },
+      { hid: 'og:url', property: 'og:url', content: 'https://portfolio-site-4kg.pages.dev/'},
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/MyIcon.ico' }],
   },
